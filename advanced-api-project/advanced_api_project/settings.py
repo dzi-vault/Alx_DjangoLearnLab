d36dac306305c6b902bd0d0316191e655b1c27d0
Tina Tettey <157775692+dzi-vault@users.noreply.github.com>
2026-02-08 14:29:46 +0000

    Set Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yzh#i0+&&!qgmnle5f-i%q)ql30t%c#e5hat729a7w8l0_y%ky'
+SECRET_KEY = 'django-insecure-q^3ofgus1r2e*kbqi9jh-ju4c-&x+_klr7wwv(1rvl9m1ry7%j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -23,5 +23,4 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-
 # Create your models here.
