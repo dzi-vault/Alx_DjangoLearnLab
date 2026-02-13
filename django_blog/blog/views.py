3ac548906c102e85286419e05297e403de6d542a
Tina Tettey <157775692+dzi-vault@users.noreply.github.com>
2026-02-13 16:01:46 +0000

    Implement the Blog's User Authentication System

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import RegisterForm
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("profile")
+    else:
+        form = RegisterForm()
+    return render(request, "blog/register.html", {"form": form})
+
+
+@login_required
+def profile_view(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+    return render(request, "blog/profile.html")
 
 # Create your views here.
